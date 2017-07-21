b2f0a2a9818b16f9c1c4506fec76062b33b01b05
lewis nyoike <nyoikelewis@gmail.com>
2017-07-21 11:09:21 +0300

    Made a get request to return all shoe and store data

--- app.rb
+++ app.rb
@@ -3,3 +3,9 @@ Bundler.require(:default)
 also_reload('lib/**/*.rb')
 
 Dir[File.dirname(__FILE__) + '/../lib/*.rb'].each { |file| require file }
+
+get '/' do
+  @stores = Store.all
+  @shoes = Shoe.all
+  erb :index
+end
