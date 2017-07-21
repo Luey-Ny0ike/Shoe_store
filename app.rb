6a537b39a8ad1ce677a7f7d6f005c33d51465f87
lewis nyoike <nyoikelewis@gmail.com>
2017-07-21 11:34:40 +0300

    Fixed all the issues

--- app.rb
+++ app.rb
@@ -9,3 +9,15 @@ get '/' do
   @shoes = Shoe.all
   erb :index
 end
+
+post '/index/shop/new' do
+  @store_name = params.fetch('name')
+  Shop.create(name: @store_name)
+  redirect '/'
+end
+
+post '/index/shoe/new' do
+  @shoe_name = params.fetch('name')
+  Shoe.create(name: @shoe_name)
+  redirect '/'
+end
